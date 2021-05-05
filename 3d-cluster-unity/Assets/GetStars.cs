2558dd2eb2174224fef7646944be08a8713c35e2
T Clowater <tasha.bartlett@gmail.com>
2021-05-05 10:18:49 -0300

    Instantiates a Star Prefab

--- 3d-cluster-unity/Assets/GetStars.cs
+++ 3d-cluster-unity/Assets/GetStars.cs
@@ -4,17 +4,23 @@ using UnityEngine;
 
 public class GetStars : MonoBehaviour
 {
+    // Reference to the Prefab
+    public GameObject Star;
     // Start is called before the first frame update
     void Start()
     {
-        GameObject star = GameObject.CreatePrimitive(PrimitiveType.Sphere);
-        star.transform.position = new Vector3(0, 1.5f, 0);    }
-
+        InitializeStarPrefab(0, 0, 0);
+    }
     // Update is called once per frame
     void Update()
     {
         
     }
 
+    void InitializeStarPrefab(float x ,float y, float z)
+    {
+        // Instantiate at position (x, y, z) and zero rotation
+        Instantiate(Star, new Vector3(x, y, z), Quaternion.identity);
+    }
 
 }

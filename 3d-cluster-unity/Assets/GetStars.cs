f1f4b98684b48277a611d325505399f3623e09f7
T Clowater <tasha.bartlett@gmail.com>
2021-05-06 16:08:59 -0300

    last working state with the stars being generated from a file

--- 3d-cluster-unity/Assets/GetStars.cs
+++ 3d-cluster-unity/Assets/GetStars.cs
@@ -24,16 +24,48 @@ public class GetStars : MonoBehaviour
         Instantiate(Star, new Vector3(x, y, z), Quaternion.identity);
     }
 
-    void getStarsFromFile()
+    void getFromFile()
     {
         string assetFileLocation = "Assets/DataFiles/snapshot.txt";
-        //var assetHadlner = Addressables.LoadAssetAsync<TextAsset>(assetFileLocation);
-        //Debug.Log(assetHadlner);
         Addressables.LoadAssetAsync<TextAsset>(assetFileLocation).Completed += handle =>
         {
-            Debug.Log(handle.Result.text);
+            var fullFile = handle.Result.text;
+            Debug.Log(fullFile);
+            var lines = fullFile.Split('\n');
+            Debug.Log(lines[0]);
+
+            for (int i = 0; i < lines.Length; i++)
+            {
+                var singleLine = lines[i].Split(',');
+                if (i != 0)
+                {
+                    Debug.Log(singleLine[0]);
+                    // Beware! y is in the vertical in unity, not z
+                    float xL = float.Parse(singleLine[0]);
+                    float yL = float.Parse(singleLine[2]);
+                    float zL = float.Parse(singleLine[1]);
+                    InitializeStarPrefab(xL, yL, zL);
+                }
+            }
+
             Addressables.Release(handle);
         };
+    }
+
+    void getStarsFromFile()
+    {
+
+        try
+        {
+            getFromFile();
+        }
+        catch
+        {
+            InitializeStarPrefab(0,0,0);
+        }
+        
+
+        
     }
 
 }

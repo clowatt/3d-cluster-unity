962162ee877d983c9aa17d20edc70f7ac7c5e85e
T Clowater <tasha.bartlett@gmail.com>
2021-05-15 10:51:55 -0300

    Getting input values for movement and rotation

--- 3d-cluster-unity/Assets/MoveCamera.cs
+++ 3d-cluster-unity/Assets/MoveCamera.cs
@@ -1,9 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class MoveCamera : MonoBehaviour
 {
+
+    PlayerActions input;
+
+    // Store the Vector2 result from CameraControl.Movement
+    Vector2 currentMovement;
+
+    // Store the Vector2 result from CameraControl.Movement
+    Vector2 currentRotation;
+
+    //Awake is called when the script is first built
+    void Awake()
+    {
+        input = new PlayerActions();
+
+        // Get input, += callback context. => lamdba expression to do something.
+        input.CameraControl.Movement.performed += ctx => currentMovement = ctx.ReadValue<Vector2>();
+        input.CameraControl.Rotation.performed += ctx => currentRotation = ctx.ReadValue<Vector2>();
+    } 
+
     // Start is called before the first frame update
     void Start()
     {
@@ -15,4 +35,20 @@ public class MoveCamera : MonoBehaviour
     {
         
     }
+
+    void HandleMovement()
+    {
+
+    }
+
+    void OnEnable()
+    {
+        input.CameraControl.Enable();
+    }
+
+    void OnDisable() 
+    {
+        input.CameraControl.Disable();
+    }
+
 }
